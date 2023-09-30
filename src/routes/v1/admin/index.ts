236d77669a76b3f3a22bc3f587d054b5e04172a3
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2023-09-30 13:52:29 +0100

    Add basic crud contract routes

--- src/routes/v1/admin/index.ts
+++ src/routes/v1/admin/index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
+import contractRoute from './contract.route'
 import isAdmin from '../../../middleware/is_admin.middleware';
 
 router.use(isAdmin);
+router.use('/contract', contractRoute)
 
 export default router;
